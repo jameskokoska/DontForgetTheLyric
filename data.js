e9619917b5db36bfe2cfb559edcc1f78b4e8fae3
jameskokoska <jameskokoska@users.noreply.github.com>
2020-08-21 02:57:33 -0400

    starts rewrite of front and back end, reads from database and new genre slection page with proper scaling

---
+++ data.js
@@ -0,0 +1,34 @@
+var genreArray= [
+    {
+        "genre": "Pop",
+        "color": "#ffcdd2",
+    },
+    {
+        "genre": "Rock",
+        "color": "#c5cae9",
+    },
+    {
+        "genre": "Throwbacks",
+        "color": "#b3e5fc",
+    },
+    {
+        "genre": "Rap",
+        "color": "#b2dfdb",
+    },
+    {
+        "genre": "Movies",
+        "color": "#dcedc8",
+    },
+    {
+        "genre": "Hip Hop",
+        "color": "#fff9c4",
+    },
+    {
+        "genre": "Dance",
+        "color": "#ffe0b2",
+    },
+    {
+        "genre": "80s",
+        "color": "#ffcdd2",
+    },
+]
