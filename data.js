01f26673e21df760c8cee7e12f528ad9b1f40621
jameskokoska <jameskokoska@users.noreply.github.com>
2020-08-21 21:21:08 -0400

    completed song selection page

--- data.js
+++ data.js
@@ -31,4 +31,199 @@ var genreArray= [
         "genre": "80s",
         "color": "#ffcdd2",
     },
-]
+];
+
+var genreSongs1 = [
+    {
+        "nameHidden": "Song 1",
+        "color":"#c5cae9",
+        "name":"Song 1 Name",
+        "file": "/song11.mp3",
+        "lyrics": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>\
+        Praesent blandit mollis lectus id varius.<br/>\
+        Curabitur vitae justo ac ligula pellentesque condimentum.<br/>\
+        Curabitur vel tempus ex. Nam sed urna quam.<br/>",
+    },
+    {
+        "nameHidden": "Song 2",
+        "color":"#b3e5fc",
+        "name":"Song 2 Name",
+        "file": "/song12.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 3",
+        "color":"#b2dfdb",
+        "name":"Song 3 Name",
+        "file": "/song13.mp3",
+        "lyrics": "",
+    },
+];
+
+var genreSongs2 = [
+    {
+        "nameHidden": "Song 1",
+        "color":"#c5cae9",
+        "name":"Song 1 Name",
+        "file": "/song21.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 2",
+        "color":"#b3e5fc",
+        "name":"Song 2 Name",
+        "file": "/song22.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 3",
+        "color":"#b2dfdb",
+        "name":"Song 3 Name",
+        "file": "/song23.mp3",
+        "lyrics": "",
+    },
+];
+
+var genreSongs3 = [
+    {
+        "nameHidden": "Song 1",
+        "color":"#c5cae9",
+        "name":"Song 1 Name",
+        "file": "/song31.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 2",
+        "color":"#b3e5fc",
+        "name":"Song 2 Name",
+        "file": "/song32.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 3",
+        "color":"#b2dfdb",
+        "name":"Song 3 Name",
+        "file": "/song33.mp3",
+        "lyrics": "",
+    },
+];
+
+var genreSongs4 = [
+    {
+        "nameHidden": "Song 1",
+        "color":"#c5cae9",
+        "name":"Song 1 Name",
+        "file": "/song41.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 2",
+        "color":"#b3e5fc",
+        "name":"Song 2 Name",
+        "file": "/song42.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 3",
+        "color":"#b2dfdb",
+        "name":"Song 3 Name",
+        "file": "/song43.mp3",
+        "lyrics": "",
+    },
+];
+
+var genreSongs5 = [
+    {
+        "nameHidden": "Song 1",
+        "color":"#c5cae9",
+        "name":"Song 1 Name",
+        "file": "/song51.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 2",
+        "color":"#b3e5fc",
+        "name":"Song 2 Name",
+        "file": "/song52.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 3",
+        "color":"#b2dfdb",
+        "name":"Song 3 Name",
+        "file": "/song53.mp3",
+        "lyrics": "",
+    },
+];
+
+var genreSongs6 = [
+    {
+        "nameHidden": "Song 1",
+        "color":"#c5cae9",
+        "name":"Song 1 Name",
+        "file": "/song61.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 2",
+        "color":"#b3e5fc",
+        "name":"Song 2 Name",
+        "file": "/song62.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 3",
+        "color":"#b2dfdb",
+        "name":"Song 3 Name",
+        "file": "/song63.mp3",
+        "lyrics": "",
+    },
+];
+
+var genreSongs7 = [
+    {
+        "nameHidden": "Song 1",
+        "color":"#c5cae9",
+        "name":"Song 1 Name",
+        "file": "/song71.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 2",
+        "color":"#b3e5fc",
+        "name":"Song 2 Name",
+        "file": "/song72.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 3",
+        "color":"#b2dfdb",
+        "name":"Song 3 Name",
+        "file": "/song73.mp3",
+        "lyrics": "",
+    },
+];
+
+var genreSongs8 = [
+    {
+        "nameHidden": "Song 1",
+        "color":"#c5cae9",
+        "name":"Song 1 Name",
+        "file": "/song81.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 2",
+        "color":"#b3e5fc",
+        "name":"Song 2 Name",
+        "file": "/song82.mp3",
+        "lyrics": "",
+    },
+    {
+        "nameHidden": "Song 3",
+        "color":"#b2dfdb",
+        "name":"Song 3 Name",
+        "file": "/song83.mp3",
+        "lyrics": "",
+    },
+];
